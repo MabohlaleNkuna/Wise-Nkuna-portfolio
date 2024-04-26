7e63a4c9c5791d3e1d51bdeedf3d37f472d0d160
wise <mabohlalenkuna@gmail.com>
2024-04-26 16:54:19 +0200

    update

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ anime.timeline({loop: true})
     scale: [14,1],
     opacity: [0,1],
     easing: "easeOutCirc",
-    duration: 800,
+    duration: 1000,
     delay: (el, i) => 800 * i
   }).add({
     targets: '.header-text',
     opacity: 0,
     duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
+    easing: "easeOutCirc",
+    delay: 900
   });
